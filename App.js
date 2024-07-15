7b3a44cbe2d26b867929ad56c33d0a95585afebc
sharma-lokesh <lokeshsharmalive@hotmail.com>
2024-07-15 13:10:39 +0530

    firstCommit

---
+++ App.js
@@ -0,0 +1,15 @@
+/*const heading = React.createElement("h1", {id: "heading"}, "Hello World from React !!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/
+
+
+const parent = React.createElement("div", {id: "parent"}, 
+React.createElement("div", {id:"child"}, 
+[React.createElement("h1", {id:"heading"}, "I'm an H1 tag"),
+React.createElement("h2", {id:"h2heading"}, "I'm an H2 tag")]
+)
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
