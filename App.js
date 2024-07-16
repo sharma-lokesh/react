e87892d2fa8858cc9a3c59bb1e6edf0cfa33278f
sharma-lokesh <lokeshsharmalive@hotmail.com>
2024-07-16 13:01:53 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 /*const heading = React.createElement("h1", {id: "heading"}, "Hello World from React !!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);*/
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id: "parent"}, 
 React.createElement("div", {id:"child"}, 
