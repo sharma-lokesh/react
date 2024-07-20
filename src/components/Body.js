53711b9bd13192de58e3de2e7f1fb17dfa12b0cf
sharma-lokesh <lokeshsharmalive@hotmail.com>
2024-07-20 22:45:00 +0530

    useState React Hooks commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,24 @@
 import ResturantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+  //react hooks variable
+  const [restaurantList, setRestaurantList] = useState(resList);
     return (
         <div className= "body">
-            <div className = "search">
-                Search
+        <div className="filter">
+          <button className="filter-btn" onClick={() => {
+            //console.log("button clicked");
+            const filteredList = restaurantList.filter((res) => res.info.avgRating > 4.4);
+            setRestaurantList(filteredList);
+          }}>
+            Top Rated Restaurant
+          </button>
             </div>
             <div className = "res-container">
-            {resList.map((restaurant) => (
+          {restaurantList.map((restaurant) => (
                     <ResturantCard key={restaurant.info.id} resData={restaurant}/>
                     )
                 )
